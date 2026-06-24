beabaa9d30f5085726c68b796463aeb6f9350684
LogicSoft-Technologies <elijahkylealexander@gmail.com>
2026-06-24 09:35:24 +0100

    E

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,13 +1,13 @@
-import HomeHero from "@/components/home/home-hero";
-import HomeStats from "@/components/home/home-stats";
-import HomeIntegratedSolutions from "@/components/home/home-intergrated-solutions";
-import HomeAboutPreview from "@/components/home/home-about-preview";
-import HomeAdvantage from "@/components/home/home-advantage";
-import HomeServices from "@/components/home/home-services";
-import HomeIndustries from "@/components/home/home-industries";
-import HomeForensics from "@/components/home/home-forensics";
-import HomeInsights from "@/components/home/home-insights";
-import HomeConsultationCta from "@/components/home/home-consultation-cta";
+import HomeHero from "@/components/Home/home-hero";
+import HomeStats from "@/components/Home/home-stats";
+import HomeIntegratedSolutions from "@/components/Home/home-intergrated-solutions";
+import HomeAboutPreview from "@/components/Home/home-about-preview";
+import HomeAdvantage from "@/components/Home/home-advantage";
+import HomeServices from "@/components/Home/home-services";
+import HomeIndustries from "@/components/Home/home-industries";
+import HomeForensics from "@/components/Home/home-forensics";
+import HomeInsights from "@/components/Home/home-insights";
+import HomeConsultationCta from "@/components/Home/home-consultation-cta";
 
 export default function Home() {
   return (
