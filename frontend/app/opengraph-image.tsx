ce4a21411856881845ffc28c8ea8277dba04c14b
Elijah Alexander <elijahkylealexander@gmail.com>
2026-08-14 01:04:15 +0100

    elijah

--- frontend/app/opengraph-image.tsx
+++ frontend/app/opengraph-image.tsx
@@ -1,6 +1,7 @@
 import { ImageResponse } from "next/og";
 import { SITE_CONFIG } from "@/lib/seo/config";
 
+export const dynamic = "force-static";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
 export const alt = SITE_CONFIG.name;
